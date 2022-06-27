023bbdabd3128373935dc35986c97c73675b6461
TheZombrex <davidfernandezperez.thezombrex@gmail.com>
2022-06-27 18:10:12 +0200

    Nueva máquina de VulnHub añadida

--- VulnHubMachines.md
+++ VulnHubMachines.md
@@ -1,4 +1,5 @@
 VulnHub Machines
 
 
-- VULNHUB-BREAKOUT ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/Wiki.svg) <a href='https://github.com/TheZombrex/machines/blob/main/docs/Breakout.pdf'>Documentación</a> ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/link.svg) <a href='https://www.vulnhub.com/entry/empire-breakout,751/'>Máquina</a>
+
+- VULNHUB-BREAKOUT ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/Wiki.svg) <a href='https://github.com/TheZombrex/machines/blob/main/docs/Breakout.pdf'>Documentación</a> ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/link.png) <a href='https://www.vulnhub.com/entry/empire-breakout,751/'>Máquina</a>
--- newpawnmachine.sh
+++ newpawnmachine.sh
@@ -42,7 +42,7 @@ else
 
 	mensaje="Nueva máquina de VulnHub añadida"
 	echo "El mensaje del commit será: $mensaje"
-        echo "$maquina ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/Wiki.svg) <a href='https://github.com/TheZombrex/machines/blob/main/docs/$documentacion'>Documentación</a> ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/link.svg) <a href='$enlace'>Máquina</a>" >> VulnHubMachines.md
+        echo "$maquina ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/Wiki.svg) <a href='https://github.com/TheZombrex/machines/blob/main/docs/$documentacion'>Documentación</a> ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/link.png) <a href='$enlace'>Máquina</a>" >> VulnHubMachines.md
         cat HTBMachines.md > README.md
 	echo "" >> README.md
         cat VulnHubMachines.md >> README.md
