51f405b39a195a9640432a5d82f07cdb8887b221
TheZombrex <davidfernandezperez.thezombrex@gmail.com>
2022-06-27 17:13:08 +0200

    Documentación de Brakout renombrada y script mejorado

--- newpawnmachine.sh
+++ newpawnmachine.sh
@@ -13,26 +13,43 @@ echo "-----------------"
 echo
 read -p "Escoge una opción: " opcion_commit
 
+echo "Introduce el nombre de la documentación"
+echo
+echo "---------------"
+echo " Ej: Breakout.pdf"
+echo "---------------"
+echo
+read -p "Nombre de la documentación: " documentacion
+
+
 if [ $opcion_commit -eq 1  ]
 then
 	mensaje="Nueva máquina de HackTheBox añadida"
 	echo "El mensaje del commit será: $mensaje"
-	echo "$maquina" >> HTBMachines.md
+	echo "$maquina <a href='https://github.com/TheZombrex/machines/blob/main/docs/$documentacion'>Documentación</a>" >> HTBMachines.md
 	cat HTBMachines.md > README.md
 	echo "" >> README.md
 	cat VulnHubMachines.md >> README.md
 
 else
+	echo "Introduce el enlace a la máquina"
+	echo
+	echo "-----------------------------------------------------------"
+	echo " Ej: https://www.vulnhub.com/entry/empire-breakout,751/"
+	echo "---------------------------------------------------------"
+	echo
+	read -p "Introduce el enlace a la máquina: " enlace
+	
 	mensaje="Nueva máquina de VulnHub añadida"
 	echo "El mensaje del commit será: $mensaje"
-        echo "$maquina" >> VulnHubMachines.md
+        echo "$maquina <a href='https://github.com/TheZombrex/machines/blob/main/docs/$documentacion'>Documentación</a> <a href='$enlace'>Máquina</a>" >> VulnHubMachines.md
         cat HTBMachines.md > README.md
 	echo "" >> README.md
         cat VulnHubMachines.md >> README.md
 
 fi
- 
-git add -A 
+
+git add -A
 
 git commit -m "$mensaje"
 
