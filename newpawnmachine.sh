45f58df3c47ccabba63439fa967dc003da727787
TheZombrex <davidfernandezperez.thezombrex@gmail.com>
2022-06-27 21:12:46 +0200

    Corregido identado script

--- newpawnmachine.sh
+++ newpawnmachine.sh
@@ -44,12 +44,12 @@ else
 
 	mensaje="Nueva máquina de VulnHub añadida"
 	echo "El mensaje del commit será: $mensaje"
-        echo "$maquina   ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/Wiki.svg) <a href='https://github.com/TheZombrex/machines/blob/main/docs/vulnhub/$documentacion'>Documentación</a>  ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/link.png) <a href='$enlace'>Máquina</a>" >> VulnHubMachines.md
-				cat Header.md > README.md
-				echo "" >> README.md
-				cat HTBMachines.md >> README.md
-				echo "" >> README.md
-        cat VulnHubMachines.md >> README.md
+  echo "$maquina   ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/Wiki.svg) <a href='https://github.com/TheZombrex/machines/blob/main/docs/vulnhub/$documentacion'>Documentación</a>  ![   ](https://github.com/TheZombrex/machines/blob/main/icons/octicons/link.png) <a href='$enlace'>Máquina</a>" >> VulnHubMachines.md
+	cat Header.md > README.md
+	echo "" >> README.md
+	cat HTBMachines.md >> README.md
+	echo "" >> README.md
+  cat VulnHubMachines.md >> README.md
 
 fi
 
